6eb9614af6ed4fef062a3149722a654274eb9e62
Vikas Saini <vikas08218@gmail.com>
2025-01-06 19:26:33 +0530

    submit

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,26 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   const theme =createTheme({
     colors:{
-      brightSun: [
-        '#fffbeb', '#fff3c6', '#ffe588', '#ffd149', '#ffbd20', 
-        '#f99b07', '#dd7302', '#b75006', '#943c0c', '#7a330d', '#461902',
+      'bright-sun': [
+        "#fffbeb", "#fff3c6", "#ffe588", "#ffd149", "#ffbd20", 
+        "#f99b07", "#dd7302", "#b75006", "#943c0c", "#7a330d", "#461902"
       ],
-      mineShaft: [
-        '#f6f6f6', '#e7e7e7', '#d1d1d1', '#b0b0b0', '#888888', 
-        '#6d6d6d', '#5d5d5d', '#4f4f4f', '#454545', '#333333', '#262626',
-      ], }
+      'mine-shaft': [
+        "#f6f6f6", "#e7e7e7", "#d1d1d1", "#b0b0b0", "#888888", 
+        "#6d6d6d", "#5d5d5d", "#4f4f4f", "#454545", "#333333", "#262626"
+      ]
+    }
+    
     })
   return (
 
     <MantineProvider theme={theme}>
      <BrowserRouter>
      <Routes>
-     <Route path="*" element={<Homepage />}/>
+     <Route path="*" element={<Homepage/>}/>
      </Routes>
     </BrowserRouter>
     </MantineProvider>
-
       );
 }
 
--- src/Header/NavLinks.tsx
+++ src/Header/NavLinks.tsx
@@ -1,16 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavLinks=()=>{
   const links=[
-  {name:"Find Jobs",url:"Find-jobs"},
-  {name:"Find Talent",url:"Find-talent"},
-  {name:"Upload Job",url:"upload-job"},
-  {name:"About us",url:"about"},
-
+  {name:"Find Jobs",url:"/find-jobs"},
+  {name:"Find Talent",url:"/find-talent"},
+  {name:"Upload Job",url:"/upload-job"},
+  {name:"About us",url:"/about"},
  ]
- return <div className="flex gap-5">
+ const location = useLocation();
+
+ return <div className="flex gap-5 text-mine-shaft-100 h-full items-center">
   {
-    links.map((link,index)=><Link key={index} to={link.url} >{link.name}</Link>)
+    links.map((link,index)=><div className={`${location.pathname==="/"+link.url?"border-bright-sun-400 text-bright-sun-400":"border-transparent"} border-t-[3px] h-full flex items-center`}>
+      <Link key={index} to={link.url} >{link.name}</Link>
+      </div>
+    )
   }
     </div>
 
