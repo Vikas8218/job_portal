615c10720ae69fb74e849f75a94aa98bebe1f720
Vikas Saini <vikas08218@gmail.com>
2025-01-07 15:26:20 +0530

    submit

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,8 +1,11 @@
 import Header from "../Header/Header";
+import DreamJob from "../LandingPage/dreamjob";
 const Homepage=()=>{
   return(
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
         <Header/>
+       <DreamJob/>
+    
     </div>
   )
 }
